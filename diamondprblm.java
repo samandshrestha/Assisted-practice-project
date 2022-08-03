66b88c8b85504b47970fc29122406ac60b1235d4
Saman Shrestha <samanshrestha1000@gmail.com>
2022-08-04 00:58:14 +0530

    commit

---
+++ diamondprblm.java
@@ -0,0 +1,28 @@
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class diamondprblm implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        diamondprblm ob = new diamondprblm(); 
+        ob.show(); 
+    } 
+}
